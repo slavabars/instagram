03b955399b54bbf62ddc0b63e900675913615789
Vyacheslav Barsukov <i@slavabars.ru>
2018-03-16 14:53:51 +1000

    Update Instagram.php

--- Instagram.php
+++ Instagram.php
@@ -14,21 +14,23 @@ class Instagram extends Model {
             if(count($htmla[1])){
                 $j=$htmla[1];
                 $json = json_decode($j);
-                if ($json->entry_data->ProfilePage[0]->user->media->nodes) {
-                    foreach ($json->entry_data->ProfilePage[0]->user->media->nodes as $node) {
-                        $instagram = \App\Instagram::find($node->id);
-                        if (is_null($instagram)) {
-                            $instagram = new \App\Instagram;
+                if ($json->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges ) {
+                    foreach ($json->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges  as $node) {
+                        $node = $node->node;
+                        $instagramm = \App\Instagram::find($node->id);
+                        if (is_null($instagramm)) {
+                            $instagramm = new \App\Instagram;
                         }
-                        $instagram->code = $node->code;
-                        $instagram->date = $node->date;
-                        $instagram->caption = isset($node->caption) ? $node->caption : '';
-                        $instagram->thumbnail_src = $node->thumbnail_src;
-                        $instagram->id = $node->id;
-                        $instagram->display_src = $node->display_src;
-                        $instagram->save();
+                        $instagramm->code = $node->shortcode;
+                        $instagramm->date = $node->taken_at_timestamp;
+                        //$instagramm->caption = count($node->edge_media_to_caption)>0 ? $node->edge_media_to_caption->edges[0]->node->text : '';
+                        $instagramm->thumbnail_src = $node->thumbnail_src;
+                        $instagramm->id = $node->id;
+                        $instagramm->display_src = $node->display_url;
+                        $instagramm->save();
                     }
                 }
+                //dd($htmla[1]);
             }
             return \App\Instagram::orderBy('id','desc')->take(12)->get();
         });
